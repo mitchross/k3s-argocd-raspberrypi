86f3a915d986a37f74d86c39a5e1064d604925c3
Mitch Ross <mitchross@users.noreply.github.com>
2024-07-11 00:21:15 -0400

    Update versions.tf

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "homelab-external"
+    organization = "vanillax"
 
     workspaces {
       name = "homelab-external"
